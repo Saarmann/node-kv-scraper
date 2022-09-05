81bf57b5d010d34b85779f88da5947eb52b4d0ba
saarmann <kaspar.saarmann@gmail.com>
2022-09-05 23:22:49 +0300

    code formatiing

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-import puppeteer from 'puppeteer';
+import cron from 'cron';
 import { realEstateData } from './data.js';
-
-async function run(url, controlPrice) {
-    const browser = await puppeteer.launch({
-        headless: false,
-        ignoreHTTPSErrors: true,
-    })
-    let page = await browser.newPage();
-    await page.goto(url, {
-        waitUntil: 'domcontentloaded',
-    });
-
-    let price = await page.$$('.col-1-4.t-1-2 .inner .object-article-details .grid .t-1-2.m-1-2 .object-price');
-
-    for (const single of price) {
-        const title = await page.evaluate(el => el.querySelector('strong').textContent, single)
-
-        const rePrice = title.replace(/\s+/g, '');
-        const finalPrice = rePrice.replace('€', '')
-
-        console.log(finalPrice);
-
-        if (parseInt(finalPrice) === parseInt(controlPrice)) {
-            console.log('sama')
-        } else {
-            console.log('Uus')
-        }
-    }
-    await page.close();
-    await browser.close();
-}
 
 async function runScraper(data) {
     data.forEach(element => {
@@ -37,4 +7,20 @@ async function runScraper(data) {
     });
 }
 
-runScraper(realEstateData);
+// runScraper(realEstateData);
+
+function handleCron() {
+    new cron.CronJob(
+        '0 */5 * * * *',
+        () => {
+            console.log(
+                `You will see this message every 5 minute ${new Date(
+                    Date.now()
+                ).toString()}`
+            );
+        },
+        null,
+        true,
+        'Europe/Tallinn'
+    )
+};
---
+++ scraper.js
@@ -0,0 +1,31 @@
+import puppeteer from 'puppeteer';
+
+async function run(url, controlPrice) {
+    const browser = await puppeteer.launch({
+        headless: false,
+        ignoreHTTPSErrors: true,
+    })
+    let page = await browser.newPage();
+    await page.goto(url, {
+        waitUntil: 'domcontentloaded',
+    });
+
+    let price = await page.$$('.col-1-4.t-1-2 .inner .object-article-details .grid .t-1-2.m-1-2 .object-price');
+
+    for (const single of price) {
+        const title = await page.evaluate(el => el.querySelector('strong').textContent, single)
+
+        const rePrice = title.replace(/\s+/g, '');
+        const finalPrice = rePrice.replace('€', '')
+
+        console.log(finalPrice);
+
+        if (parseInt(finalPrice) === parseInt(controlPrice)) {
+            console.log('sama')
+        } else {
+            console.log('Uus')
+        }
+    }
+    await page.close();
+    await browser.close();
+}
