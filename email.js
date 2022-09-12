68a268fe44e236b0ce4ce7a951dab6056f43de02
saarmann <kaspar.saarmann@gmail.com>
2022-09-12 22:38:26 +0300

    set up SendGrid

---
+++ email.js
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import sgMail from '@sendgrid/mail';
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+    to: 'kaspar.saarmann@mail.ee', // Change to your recipient
+    from: 'kaspar.saarmann@mail.ee', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+    .send(msg)
+    .then(() => {
+        console.log('Email sent')
+    })
+    .catch((error) => {
+        console.error(error)
+    })
